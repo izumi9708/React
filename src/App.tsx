be8eff576f05cc57611435fbb5b41a8b1d115d85
izumi0978 <izuizumen1108@gmail.com>
2023-08-14 12:29:11 +0900

    0814

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,29 @@
-import * as React           from 'react';
-import {useState,useEffect} from 'react';
-import CountDownTimer       from './CountDownTimer';
-import ToggleSwitch         from './ToggleSwitch';
-import {numbers}            from './ts/NumberList';
-import ColorPicker          from './ColorPicker';
-import FilteredList         from './FilteredList';
-import {emailList}          from './ts/FilteredList';
-import ShoppingCart         from './ShoppingCart';
-import {productsData}       from './ts/ShoppingCart';
-import SearchAccount        from './SearchGithubAccount';
+import * as React  from 'react';
+import './App.css';
+import App1 from './App1/App1';
+import Top from './Top';
+
+import {BrowserRouter,Switch,Route,Link} from 'react-router-dom';
+
 
 function App(){
-  // useEffect(() => {
-  //   fetch('https://api.github.com/users/izumi9708')
-  //   .then(res => res.json())
-  //   .then(res => console.log(res))
-  // })
-
   return (
     <>
-      <CountDownTimer/>
-      <ToggleSwitch/>
-      <ColorPicker/>
-      <FilteredList list={emailList}/>
-      <ShoppingCart products={productsData}/>
-      <SearchAccount/>
+    <BrowserRouter>
+      <div className="header-menu">
+        <p>選択してください</p>
+        <Link className="link-item" to="./Top">トップ</Link>
+        <Link className="link-item" to="/App1/App1">App1へ</Link>
+      </div>
+
+
+      <Switch>
+        <Route exact path="./Top"><Top/></Route> 
+        <Route path="/App1/App1"><App1/></Route>
+      </Switch>
+
+    </BrowserRouter>
+
     </>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './css/style.css';
+import './style.css';
 
 import App from './App';
 
