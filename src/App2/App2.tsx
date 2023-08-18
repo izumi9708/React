0a00d1dd0a02e3d7c533a2298b2b409c8f769922
izumi0978 <izuizumen1108@gmail.com>
2023-08-18 22:11:53 +0900

    0818

--- src/App2/App2.tsx
+++ src/App2/App2.tsx
@@ -1,12 +1,15 @@
 import * as React  from 'react';
 
 import WeatherApp from './WeatherApp';
+import Quiz from './Quiz';
+import {quizData} from './ts/Quiz';
 
 
 function App2(){
   return (
     <>
       <WeatherApp/>
+      <Quiz data={quizData}/>
     </>
   )
 }
