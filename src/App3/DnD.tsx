6c4f1287f3954a85343a4f8c2f9dda858a7bb6fd
izumi0978 <izuizumen1108@gmail.com>
2023-08-30 19:58:05 +0900

    0830

--- src/App3/DnD.tsx
+++ src/App3/DnD.tsx
@@ -1,6 +1,7 @@
 import * as React  from 'react';
 import {dndList} from './ts/DnDList';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import type { DroppableProvided, DraggableProvided } from 'react-beautiful-dnd';
 import {useState,useEffect} from 'react';
 
 import './css/DnD.css';
@@ -9,18 +10,33 @@ import { numbers } from '../ts/NumberList';
 function DnD(){
   const [list,setList] = useState(dndList);
 
-  const dragEnd = (event) => {
-    const draggableId = event.draggableId;
-    const items = Array.from(list);
-    const index_num = items.find((val,index) => {
-      if(Number(draggableId) === val.id){
-        return index;
-      }
-    });
-    const [dragItem] = items.splice(index_num,1);
-    items.splice(event.destination.index,0,dragItem);
-    
-    setList(items);
+  type DragEnd = (event:EventObj) => void;
+
+  type EventObj = {
+    draggableId:string;
+    index:number;
+    destination:EventObj
+  }
+
+  const dragEnd:DragEnd = (event) => {
+    if(event.destination){
+      const draggableId = (event).draggableId;
+      const items = Array.from(list);
+      const index_num = items.findIndex((val,index) => {
+        if(Number(draggableId) === val.id){
+          return true;
+        }
+      });
+
+      const [dragItem] = items.splice(index_num,1);
+      console.log(dragItem)
+      items.splice(event.destination.index,0,dragItem);
+      
+      setList(items);
+
+    }else {
+      alert('枠内にドロップしてください');
+    }
   }
   
 
@@ -29,11 +45,11 @@ function DnD(){
       ドラッグ&ドロップ<span className="file-name">(DnD.tsx)</span>
       <DragDropContext onDragEnd={dragEnd}>
         <Droppable droppableId="dnd-list"　type="droppableItem">
-          {(provided) => (
+          {(provided:DroppableProvided) => (
             <ul className="dnd-area" ref={provided.innerRef} {...provided.droppableProps}>
               {list.map((item, index) => (
                 <Draggable draggableId={item.id.toString()} key={item.id.toString()} index={index}>
-                  {(draggableProvided) => (
+                  {(draggableProvided:DraggableProvided) => (
                     <li
                       className="dnd-item"
                       ref={draggableProvided.innerRef}
