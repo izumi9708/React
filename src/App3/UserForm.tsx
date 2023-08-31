51979a90c33ba082c879cba0e18d0bb7913ae9f8
izumi0978 <izuizumen1108@gmail.com>
2023-08-31 16:38:03 +0900

    0831

--- src/App3/UserForm.tsx
+++ src/App3/UserForm.tsx
@@ -12,28 +12,104 @@ import './css/UserForm.css';
 // ユーザーが入力した値をフォームの送信後にクリアします。
 
 function UserForm(){
+
+  type DisplayPass  = (event:React.MouseEvent<HTMLSpanElement>) => void;
+  const displayPass:DisplayPass = (event) => {
+    const {target} = event;
+    const open  = document.querySelector('.open');
+    const close = document.querySelector('.close');
+    const input = (event.target as HTMLElement).closest('label').querySelector('input');
+    input.type === 'password' ? input.type = 'text' : input.type = 'password';
+
+    if((target as HTMLElement).classList.contains('open')){
+      (target as HTMLElement).style.display = 'none';
+      (close as HTMLElement).style.display = 'block';
+
+    }else {
+      (target as HTMLElement).style.display = 'none';
+      (open as HTMLElement).style.display = 'block';
+    }
+
+  }
+
+  const sendForm = () => {
+    const name = document.querySelector('input[name="name"]') as HTMLInputElement;
+    const mail = document.querySelector('input[name="mail"]') as HTMLInputElement;
+    const pass = document.querySelector('input[name="pass"]') as HTMLInputElement;
+
+    let errCount = 0;
+
+    if(name.value == ''){
+      const err    = name.closest('div').querySelector('.err');
+      const errMsg = document.createElement('span');
+      errMsg.classList.add('err')
+      errMsg.textContent = '入力してください';
+
+      if(!err)name.closest('div').appendChild(errMsg);
+      errCount++;
+    }
+
+    if(name.value == ''){
+      const err    = name.closest('div').querySelector('.err');
+      const errMsg = document.createElement('span');
+      errMsg.classList.add('err')
+      errMsg.textContent = '入力してください';
+
+      if(!err)name.closest('div').appendChild(errMsg);
+      errCount++;
+    }
+
+    if(name.value == ''){
+      const err    = name.closest('div').querySelector('.err');
+      const errMsg = document.createElement('span');
+      errMsg.classList.add('err')
+      errMsg.textContent = '入力してください';
+
+      if(!err)name.closest('div').appendChild(errMsg);
+      errCount++;
+    }
+  }
+
   return (
     <div className="user-form-wrap wrap">
-      ユーザーフォーム
+      ユーザーフォーム（作成中）
       <div className="user-form-content">
         <div className="form-item">
           <p>ユーザー名</p>
           <label>
-            <input type="text"/>
+            <input type="text" placeholder="例）山田太郎" name="name"/>
           </label>
         </div>
         <div className="form-item">
           <p>メールアドレス</p>
           <label>
-            <input type="text"/>
+            <input type="text" placeholder="例）xxxx@xxx.com" name="mail"/>
           </label>
         </div>
         <div className="form-item">
           <p>パスワード</p>
           <label>
-            <input type="password"/>
+            <input type="password" placeholder="半角英数字" name="pass"/>
+            <span 
+              className="material-symbols-outlined close"
+              onClick={displayPass}
+            >
+              visibility
+            </span>
+            <span 
+              className="material-symbols-outlined open"
+              onClick={displayPass}
+            >
+              visibility_off
+            </span>       
           </label>
         </div>
+
+        <div className="form-btn-wrap">
+          <button type="button" className="form-btn"
+            onClick={sendForm}
+          >確認</button>  
+        </div> 
       </div>
     </div>
   )
